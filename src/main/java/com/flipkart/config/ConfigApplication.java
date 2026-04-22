9d0792e57e2cf4ad2de7dbba0f5587d94b62a8d0
Venkata Sivaprasad <sivagandikota888m@gmail.com>
2026-04-22 22:23:00 +0530

    test

--- src/main/java/com/flipkart/config/ConfigApplication.java
+++ src/main/java/com/flipkart/config/ConfigApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfigApplication {
-
+//start
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
 	}
